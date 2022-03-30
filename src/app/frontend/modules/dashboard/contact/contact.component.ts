bd98f10b3fd9f49fe122e44a65466c5e6d22c869
sahdev007 <sahdev.pathania@pulseplaydigital.com>
2022-03-30 17:24:47 +0530

    contact page task complete

--- src/app/frontend/modules/dashboard/contact/contact.component.ts
+++ src/app/frontend/modules/dashboard/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from './../../../services/data.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  keyData: any;
+  socialData: any;
+  constructor(public dataService: DataService) { }
 
   ngOnInit(): void {
+    this.getAllKeyContacts();
+    this.getAllSocialLinks();
   }
-
+/**
+ * Get All Key Contacts
+ */
+  async getAllKeyContacts() {
+    let action: string = 'all-contact';
+    await this.dataService.getData(action).pipe(
+      map((res:any) => {
+        return res.data.filter((item:any) => {
+          if(item?.type === "KeyContact"){
+            return item;
+          }
+        });
+       })
+    ).subscribe((data: any) => {
+      this.keyData = data;
+    })
+  }
+  
+  async getAllSocialLinks() {
+    let action: string = 'all-contact';
+    await this.dataService.getData(action).pipe(
+      map((res:any) => {
+        return res.data.filter((item:any) => {
+          if(item?.type === "SbupChannel"){
+            return item;
+          }
+        });
+       })
+    ).subscribe((data: any) => {
+      this.socialData = data;
+    })
+  }
 }
