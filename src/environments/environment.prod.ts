750d3b63b382f6357a5be6b87fc57ed139b1cc0d
sahdev007 <sahdev.pathania@pulseplaydigital.com>
2022-06-10 17:32:34 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://swadeshdigital.com/sbsAlumni/public/api",
-  imgUrl: "https://swadeshdigital.com/sbsAlumni/public/"
+  apiUrl: "https://swadeshdigital.com/sbsAlumni/api",
+  imgUrl: "https://swadeshdigital.com/sbsAlumni/"
 };
