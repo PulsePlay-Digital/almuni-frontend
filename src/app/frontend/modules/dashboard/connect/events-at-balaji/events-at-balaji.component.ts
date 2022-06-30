8c77889a85d3ed717588c6739cbd81f5d738324c
sahdev007 <sahdev.pathania@pulseplaydigital.com>
2022-06-30 22:23:26 +0530

    refine connect page select form issue June 30

--- src/app/frontend/modules/dashboard/connect/events-at-balaji/events-at-balaji.component.ts
+++ src/app/frontend/modules/dashboard/connect/events-at-balaji/events-at-balaji.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import * as moment from "moment";
-import { map } from "rxjs/operators";
 import { TokenInterceptor } from "./../../../../core/token.interceptor";
 import { environment } from "./../../../../../../environments/environment";
 import { DataService } from "./../../../../services/data.service";
+import * as moment from "moment";
 
 @Component({
   selector: "app-events-at-balaji",
@@ -15,9 +14,12 @@ export class EventsAtBalajiComponent implements OnInit {
   imgPath: any;
   loading: boolean = false;
   speakEventForm: FormGroup | any;
+  workshopForm: FormGroup | any;
+  facultyForm: FormGroup | any;
   workShopField: boolean = false;
   speakerFacultyFields: boolean = false;
   submitted: boolean = false;
+  onChangeEvent: any;
 
   constructor(
     public fb: FormBuilder,
@@ -29,31 +31,67 @@ export class EventsAtBalajiComponent implements OnInit {
 
   ngOnInit(): void {
     this.loading = true;
-    this.buildForm();
+    this.buildWorkshopForm();
+    this.buildFacultyForm();
   }
 
   /**
    * Build Form Data
    */
-  buildForm() {
-    this.speakEventForm = this.fb.group({
-      event_id: [3],
-      eventName: [""],
-      eventCategory: ["", Validators.required],
+  buildWorkshopForm() {
+    this.workshopForm = this.fb.group({
+      // event_id: [3],
+      // eventName: [""],
+      eventCategory: ["workshop"],
       topicWorkshop: ["", Validators.required],
       academicCategory: ["", Validators.required],
       dateFrom: ["", Validators.required],
-      dateTo: ["", Validators.required],
+      dateTo: ["", ],
       description: ["", Validators.required],
-      status: ['unapproved']
     });
   }
 
+  buildFacultyForm() {
+    this.facultyForm = this.fb.group({
+      eventCategory: [this.onChangeEvent == "faculty" ? "faculty" : "speaker"],
+      dateFrom: ["", Validators.required],
+      dateTo: ["",],
+      description: ["", Validators.required],
+    },
+    { validator: this.dateLessThan('dateFrom', 'dateTo') }
+    );
+  }
+
+  dateLessThan(from: string, to: string) {
+    return (group: FormGroup): { [key: string]: any } => {
+      if (group.controls[to].value === '') {
+        return {};
+      }
+      let f = group.controls[from];
+      let t = group.controls[to];
+      let startTime = moment(
+        moment(f.value).format('YYYY-MM-DD')
+      );
+      let endTime = moment(
+        moment(t.value).format('YYYY-MM-DD')
+      );
+      if (startTime.isSameOrBefore(endTime)) {
+        return {};
+      } else {
+        return {
+          dates: 'Date from should be less than Date to'
+        };
+      }
+    };
+  }
   /**
    * Get all Form controls
    */
   get f() {
-    return this.speakEventForm.controls;
+    return this.workshopForm.controls;
+  }
+  get s() {
+    return this.facultyForm.controls;
   }
 
   /**
@@ -62,15 +100,21 @@ export class EventsAtBalajiComponent implements OnInit {
    */
   selectCategory(e: any) {
     let event = e?.target?.value;
+    this.onChangeEvent = event;
+    console.log(this.onChangeEvent);
     if (event == "workshop") {
       this.workShopField = true;
-      this.speakerFacultyFields = true;
+      this.speakerFacultyFields = false;
+      this.buildWorkshopForm();
     } else if (event == "faculty" || event == "speaker") {
       this.speakerFacultyFields = true;
       this.workShopField = false;
+      this.buildFacultyForm();
     } else {
       this.workShopField = false;
       this.speakerFacultyFields = false;
+      this.buildWorkshopForm();
+      this.buildFacultyForm();
     }
   }
 
@@ -78,14 +122,14 @@ export class EventsAtBalajiComponent implements OnInit {
    * Function to submit form data
    * @returns
    */
-  async submit() {
+  async addWorkshop() {
     let action: string = "create-eventCat";
     this.submitted = true;
-    if (this.speakEventForm.invalid) {
+    if (this.workshopForm?.invalid) {
       return;
     } else {
       await this.dataService
-        .postData(action, this.speakEventForm.value)
+        .postData(action, this.workshopForm?.value)
         .subscribe(
           (res: any) => {
             if (res?.status == 200) {
@@ -97,6 +141,26 @@ export class EventsAtBalajiComponent implements OnInit {
             this.notify.notificationService.openFailureSnackBar(error);
           }
         );
+    }
+  }
+
+  async addFaculty() {
+    let action: string = "create-eventCat";
+    this.submitted = true;
+    if (this.facultyForm.invalid) {
+      return;
+    } else {
+      await this.dataService.postData(action, this.facultyForm.value).subscribe(
+        (res: any) => {
+          if (res?.status == 200) {
+            this.notify.notificationService.openSuccessSnackBar(res?.message);
+            location.reload();
+          }
+        },
+        (error) => {
+          this.notify.notificationService.openFailureSnackBar(error);
+        }
+      );
     }
   }
 }
