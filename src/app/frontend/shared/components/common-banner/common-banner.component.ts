47baaa82f7dee51b8b755d6b6c7365a6b5659e1a
sahdev007 <sahdev.pathania@pulseplaydigital.com>
2022-08-02 12:52:56 +0530

    add collaborate pages changes

---
+++ src/app/frontend/shared/components/common-banner/common-banner.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-common-banner',
+  templateUrl: './common-banner.component.html',
+  styleUrls: ['./common-banner.component.scss']
+})
+export class CommonBannerComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
